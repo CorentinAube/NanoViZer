011ed4729362d5f79e1887bb5df7b2a53eab0c24
Corentin Aubé <corentinaube@Mac.lan>
2026-03-31 22:29:58 +0200

    img and installation

--- src/install_packages.sh
+++ src/install_packages.sh
@@ -1,3 +1,6 @@
 pip3 install Flask
 pip3 install flask-cors
 pip3 install pysam
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew install bedtools
